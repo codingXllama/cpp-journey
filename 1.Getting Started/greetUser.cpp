cb617b1d6d792f5271c1b459cea37005700763ab
codingXllama <osamaalchalabi2@gmail.com>
2020-07-01 23:51:56 -0400

    user IO in cpp

---
+++ 1.Getting Started/greetUser.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    string userName;
+    cout << "Enter your name: ";
+    cin >> userName;
+    cout << "Hello there " << userName << endl;
+    return 0;
+}
